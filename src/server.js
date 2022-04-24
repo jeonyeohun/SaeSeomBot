815654d3b076a77f0e2eb50b465eaf1bd1f8d30f
yeohun.jeon <yeohun.jeon@linecorp.com>
2022-04-24 17:18:08 +0900

    :fire: Remove logger

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const fs = require('fs');
 const app = express();
 const haksik = require('./api/haksik');
 const moms = require('./api/moms');
@@ -8,55 +7,20 @@ const bus = require('./api/bus');
 const schedule = require('node-schedule');
 const covid = require('./api/covid');
 
-const counterData = require('./models/countData');
-const { getToday } = require('./utils/dateParser');
 const { updateHaksikData } = require('./services/dailyHaksikDataGenerator');
-
-let counter = { ...counterData };
-
-app.use(function (req, res, next) {
-  const type = req.url.slice(req.url.lastIndexOf('/') + 1);
-  if (counter[type] != undefined) {
-    counter[type]++;
-    counter[type + 'Total']++;
-  }
-  next();
-});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/moms', moms);
-app.use('/haksik', haksik);
+app.use('meal/moms/today', moms)
+app.use('meal/moms/tomorrow', moms)
+app.use('meal/haksik/today', haksik)
+app.use('meal/haksik/tomorrow', haksik)
 app.use('/weather', weather);
 app.use('/bus', bus);
 app.use('/covid', covid);
 
 schedule.scheduleJob('1 0 * * * *', function () {
   updateHaksikData();
-});
-
-schedule.scheduleJob('1 59 * * * *', function () {
-  const hrs = new Date().getHours();
-  const dir = '../log/' + getToday();
-  const fileDir = '/' + hrs + '.json';
-
-  if (!fs.existsSync(dir)) fs.mkdirSync(dir);
-  fs.writeFileSync(dir + fileDir, JSON.stringify(counter), { flag: 'w' });
-
-  counter =
-    hrs != 23
-      ? {
-          ...counter,
-          weather: 0,
-          toHandong: 0,
-          toYangdeok: 0,
-          haksik: 0,
-          moms: 0,
-          covid: 0,
-        }
-      : {
-          ...counterData,
-        };
 });
 
 app.listen(3000, () => console.log('node on 3000'));
--- src/utils/dateParser.js
+++
@@ -1,8 +0,0 @@
-module.exports.getToday = () => {
-  let date = new Date();
-  let year = date.getFullYear();
-  let month = ('0' + (1 + date.getMonth())).slice(-2);
-  let day = ('0' + date.getDate()).slice(-2);
-
-  return year + '-' + month + '-' + day;
-};
